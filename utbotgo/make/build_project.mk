54e246d572a8eda5b2d00a24ab5f190ea0d59fdb
Kuzivanov Sergey <rep78398@gmail.com>
2021-10-30 12:13:14 +0200

    Add comments in makefiles

--- utbotgo/make/build_project.mk
+++ utbotgo/make/build_project.mk
@@ -1,7 +1,11 @@
 targets := $(shell ls -A $(TEMPLATES_DIR))
 
+# This is function for creating templated rule. Recipe is reading content from
+# file-template, replacing all variables to corresponding values from makefile
+# `config.mk` by using `$(SUB_PROGRAM_DIR)/$(SUB_PROGRAM_NAME)` and writing
+# result to file-target
 define rule_for_target
-$(1): $(TEMPLATES_DIR)/$(1)
+$(1): $(TEMPLATES_DIR)/$(1) $(SUB_PROGRAM_DIR)/$(SUB_PROGRAM_NAME)
 	cat $$^ | $(SUB_PROGRAM_DIR)/$(SUB_PROGRAM_NAME) $(MAKE_DIR)/config.mk > $$@
 
 endef
