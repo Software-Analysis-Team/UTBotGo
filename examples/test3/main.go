8df9aa114b4cf5d563a9081fe1f5d43120ed9c9d
Kuzivanov Sergey <rep78398@gmail.com>
2022-05-03 20:45:58 +0300

    Update test3

--- examples/test3/main.go
+++ examples/test3/main.go
@@ -1,5 +1,15 @@
 package test4
 
+func f(n int) (res string) {
+    if n > 5 {
+        return "n > 5, sorry"
+    }
+    for i := 0; i < n; i++ {
+        res = res + "*"
+    }
+    return
+}
+
 func g(a byte, b rune, c uint32) (d int) {
     d = int(a)
     if d < int(b) {
