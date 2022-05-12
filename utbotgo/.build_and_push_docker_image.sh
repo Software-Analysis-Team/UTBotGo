73c2daeeeaad566fba4dfd13708d7c4a8e091c9f
Kuzivanov Sergey <rep78398@gmail.com>
2022-05-12 10:34:30 +0300

    Update GitHub Action

--- utbotgo/.build_and_push_docker_image.sh
+++ utbotgo/.build_and_push_docker_image.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 source .env
-if [ -n "${UTBOTGO_OLD_DOCKER_IMAGE+x}" ]; then
-    docker pull "$UTBOTGO_OLD_DOCKER_IMAGE"
-fi
 make .docker_image > /dev/null || ( \
     echo "$1" | docker login ghcr.io -u sergeykuz1001 --password-stdin; \
     docker build -t "$UTBOTGO_DOCKER_IMAGE" .; \
